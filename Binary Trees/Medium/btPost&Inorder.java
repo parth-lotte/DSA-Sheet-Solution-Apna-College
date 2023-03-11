6994d27d814915ea6e971cb38462c8e580b5e4ae
Parth Lotte <parth33998@gmail.com>
2023-03-11 15:33:55 +0530

    BT using Post and In order

---
+++ Binary Trees/Medium/btPost&Inorder.java
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    HashMap<Integer, Integer> hm= new HashMap<>();
+        int preIndex=0;
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        for(int i=0;i<inorder.length;i++)
+        {
+            hm.put(inorder[i],i);
+            
+        }
+        return build(preorder, inorder, 0, inorder.length-1);
+    }
+
+    public TreeNode build(int [] preorder, int[] inorder, int start, int end )
+    {
+        if(start>end)
+        {
+            return null;
+        }
+
+        TreeNode root= new TreeNode(preorder[preIndex++]);
+
+        if(root==null)
+        {
+            return null;
+        }
+
+        if(start==end)
+        return root;
+
+        int index= hm.get(root.val);
+        root.left= build(preorder, inorder, start, index-1);
+        root.right= build(preorder, inorder, index+1, end);
+
+        return root;
+
+    }
+}
